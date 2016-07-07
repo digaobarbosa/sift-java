05bab8e7557dce4714cfb84ee61561d82b5c48a5
Vinicius Lopes <viniciuslopeslps@gmail.com>
2016-07-07 16:12:35 -0300

    geting back sift score

--- src/main/java/com/mcac0006/siftscience/SiftScienceHelper.java
+++ src/main/java/com/mcac0006/siftscience/SiftScienceHelper.java
@@ -124,18 +124,6 @@ public class SiftScienceHelper {
 		}
 	}
 
-	public static void main(String[] args) {
-		SiftScienceHelper h = new SiftScienceHelper("d37cb8a2f0281d29");
-		Event event = new Event("Teste") {
-			@Override
-			public String getApiKey() {
-				return "d37cb8a2f0281d29";
-			}
-		};
-		event.addCustomField("$user_id","viniciuslopeslps@gmail.com");
-		SiftScienceResponse send = h.send(event);
-		System.out.println("scoreSift: " + send.getScore_response().getScore());
-	}
 
 	/**
 	 * Retrieve a risk assessment of a particular user. This is particularly useful to consult with Sift Science 
